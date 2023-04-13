853228b44c734f6c4000647bbda9d0c632f02918
mohaiminulislam17 <130627813+mohaiminulislam17@users.noreply.github.com>
2023-04-13 17:49:15 -0400

    Web_Project

---
+++ config/settings.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoDBUrl: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.fbp8kdr.mongodb.net/sample_geospatial`,
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT;
+
+const app = express();
+
+// middleware for bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// get settings
+const settings = require("./config/settings");
+
+app.set("view engine", "hbs");
+app.set("view engine", "ejs");
+
+// mongo db url
+const db = settings.mongoDBUrl;
+
+// attempt to connect with DB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected successfully."))
+  .catch((err) => console.log(err));
+
+// Get profile routes
+const profile = require("./routes/api/profile");
+
+app.get("/", (req, res) => {
+  res.send("Project is Running");
+});
+
+// actual routes
+app.use("/api/profile", profile);
+
+// server listening
+app.listen(port, () => console.log(`App running at port : ${port}`));
